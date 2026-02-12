a81f2c84ab3eb51d5336e16872f988a75eda5d46
Ebrahim Byagowi <ebrahim@gnu.org>
2026-02-13 00:16:59 +0330

    Make it check the URLs in parallel

--- app/src/main/java/ir/ammari/rasad/MainActivity.java
+++ app/src/main/java/ir/ammari/rasad/MainActivity.java
@@ -3,80 +3,81 @@ package ir.ammari.rasad;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.media.MediaPlayer;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends Activity {
+    final Map<String, String> sites = new HashMap<>() {{
+        put("GitHub Gists", "https://gist.githubusercontent.com/xmha97/94f6ba425d9874179fdd73fc0e2dc899/raw/e3a7ca5109c651f711000b3a02b4e032bd1d695f/status");
+        put("GitHub Repository", "https://raw.githubusercontent.com/xmha97/test/refs/heads/main/status");
+        put("GitHub Releases", "https://github.com/xmha97/test/releases/download/v1.0.0/status");
+        put("GitHub Pages", "https://xmha97.github.io/status");
+        put("Codeberg Repository", "https://codeberg.org/xmha97/test/raw/branch/main/status");
+        put("Codeberg Releases", "https://codeberg.org/xmha97/test/releases/download/v1.0.0/status");
+        put("GitLab", "https://gitlab.com/xmha97/test/-/raw/master/status");
+        put("DropBox", "https://www.dropbox.com/scl/fi/dovory2z1y9xnj6kxwyq7/status?rlkey=48kb8gpm3fjnx76oglv1bm3u0&st=hy5uz0th&dl=1");
+        put("Archive", "https://archive.org/download/xmha97/status");
+        put("Pastebin", "https://pastebin.com/raw/ER5BRSx7");
+        put("HamGit", "https://hamgit.ir/xmha97/test/-/raw/main/status");
+        put("AbreHamrahi", "https://abrehamrahi.ir/o/public/EaGlAEy6");
+    }};
+    final Map<String, Boolean> result = new HashMap<>();
 
-    private Boolean testURL(String URL) {
-        Boolean[] result = {false};
-        Thread thread = new Thread(() -> {
-            try {
-                java.net.URL url = new java.net.URL(URL);
-                InputStream inputStream = url.openStream();
+    private void testURL(TextView textView, String name, URL url) {
+        new Thread(() -> {
+            boolean caseResult = false;
+            try (InputStream inputStream = url.openStream()) {
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                 byte[] buf = new byte[1024];
                 int readLen;
                 while ((readLen = inputStream.read(buf)) != -1) {
                     outputStream.write(buf, 0, readLen);
                 }
-
-                if (outputStream.toString().strip().equals("200")) {
-                    result[0] = true;
-                } else {
-                    result[0] = false;
-                }
-
-
+                if (outputStream.toString().strip().equals("200")) caseResult = true;
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        });
-        thread.start();
+            final var finalResult = caseResult;
+            runOnUiThread(() -> {
+                result.put(name, finalResult);
+                displayResult(textView);
+            });
+        }).start();
+    }
 
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    void displayResult(TextView textView) {
+        final var text = new StringBuilder();
+        text.append("Begin\n\n\n");
+        for (Map.Entry<String, String> entry : sites.entrySet()) {
+            final var key = entry.getKey();
+            text.append(key);
+            if (result.containsKey(key)) {
+                text.append(" - ");
+                text.append(result.get(key));
+                text.append("\n");
+            } else text.append("…\n");
         }
-
-        return result[0];
+        textView.setText(text);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final TextView view = new TextView(this);
-        final StringBuilder outputString = new StringBuilder("\n\n\nBegin\n");
-        view.setText(outputString);
-        setContentView(view);
-
-        Map<String, String> sites = new HashMap<>();
-        sites.put("GitHub Gists", "https://gist.githubusercontent.com/xmha97/94f6ba425d9874179fdd73fc0e2dc899/raw/e3a7ca5109c651f711000b3a02b4e032bd1d695f/status");
-        sites.put("GitHub Repository", "https://raw.githubusercontent.com/xmha97/test/refs/heads/main/status");
-        sites.put("GitHub Releases", "https://github.com/xmha97/test/releases/download/v1.0.0/status");
-        sites.put("GitHub Pages", "https://xmha97.github.io/status");
-        sites.put("Codeberg Repository", "https://codeberg.org/xmha97/test/raw/branch/main/status");
-        sites.put("Codeberg Releases", "https://codeberg.org/xmha97/test/releases/download/v1.0.0/status");
-        sites.put("GitLab", "https://gitlab.com/xmha97/test/-/raw/master/status");
-        sites.put("DropBox", "https://www.dropbox.com/scl/fi/dovory2z1y9xnj6kxwyq7/status?rlkey=48kb8gpm3fjnx76oglv1bm3u0&st=hy5uz0th&dl=1");
-        sites.put("Archive", "https://archive.org/download/xmha97/status");
-        sites.put("Pastebin", "https://pastebin.com/raw/ER5BRSx7");
-        sites.put("HamGit", "https://hamgit.ir/xmha97/test/-/raw/main/status");
-        sites.put("AbreHamrahi", "https://abrehamrahi.ir/o/public/EaGlAEy6");
-
+        final var textView = new TextView(this);
+        displayResult(textView);
+        setContentView(textView);
         for (Map.Entry<String, String> entry : sites.entrySet()) {
-            outputString.append("\n").append(entry.getKey() + " - " + testURL(entry.getValue()).toString());
-            view.setText(outputString);
-            MediaPlayer mp = MediaPlayer.create(this, R.raw.notification_simple_01);
-            mp.start();
+            try {
+                testURL(textView, entry.getKey(), new URL(entry.getValue()));
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
         }
-
-        outputString.append("\n\n").append("End");
-        view.setText(outputString);
     }
 }
